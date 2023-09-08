ddfcb61607edfc67f0f506c69c7e4bf7c578e88e
EC2 Default User <ec2-user@ip-172-31-30-222.ap-southeast-2.compute.internal>
2023-09-08 04:59:42 +0000

    8/9

---
+++ backupcfg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+jobs = {"job700" : "/home/ec2-user/environment/ictpng302-vedtss-Luka-Baudewyns/test/file1",
+        "job1" : "/home/ec2-user/environment/ictpng302-vedtss-Luka-Baudewyns/test/dir1"}
+        
+backupDir = "/home/ec2-user/environment/ictpng302-vedtss-Luka-Baudewyns/backups"
+
+backupLog = "/home/ec2-user/environment/ictpng302-vedtss-Luka-Baudewyns/backup.log"
+
+email = {}
