f67886b0bdae4617b5c517745e6ecfa05ecef2ab
EC2 Default User <ec2-user@ip-172-31-30-222.ap-southeast-2.compute.internal>
2023-10-06 03:10:57 +0000

    6/10/2023

--- backup.py
+++ backup.py
@@ -4,34 +4,86 @@ import sys
 import os 
 import pathlib
 import shutil
-from backupcfg import jobs, backupDir
+import smtplib
+from backupcfg import jobs, backupDir, backupLog, smtp
 from datetime import datetime
 
+def writeLogMessage(logMessage, dateTimeStamp, isSuccess):
+   
+    try:
+        file = open(backupLog, "a")
+        
+           # write success or failure message depending upon job outcome
+        if isSuccess:
+            file.write(f"SUCCESS {dateTimeStamp} {logMessage}\n")
+        else:
+            file.write(f"FAILURE {dateTimeStamp} {logMessage}\n")
+            
+        file.close()
+        
+    except:
+        print("ERROR: File.")
+
+def errorHandler(errorMessage, dateTimeStamp):
+    print(errorMessage)
+    writeLogMessage(errorMessage, dateTimeStamp, False)
+    sendEmail(errorMessage)
+    
+# append all error messages to email and send
+def sendEmail(message):
+
+    email = 'To: ' + smtp["recipient"] + '\n' + 'From: ' + smtp["sender"] + '\n' + 'Subject: Backup Error\n\n' + message + '\n'
+
+    # connect to email server and send email
+    try:
+        smtp_server = smtplib.SMTP(smtp["server"], smtp["port"])
+        smtp_server.ehlo()
+        smtp_server.starttls()
+        smtp_server.ehlo()
+        smtp_server.login(smtp["user"], smtp["password"])
+        smtp_server.sendmail(smtp["sender"], smtp["recipient"], email)
+        smtp_server.close()
+    except Exception as e:
+        print("ERROR: An error occurred.")
+    
+    # 
 def main():
+
+    dateTimeStamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    
+    # check for number of CLI arguments
     argCount = len(sys.argv)
     if not argCount == 2:
-        print("ERROR: job not specified")
+        #print(f"ERROR: job not specified.")
+        errorHandler(f"ERROR: job not specified", dateTimeStamp)
     else:
-        job = sys.argv[1]
-        if not job in jobs:
-            print(f"ERROR: job {job} does not exist")
+        # loop through all CLI arguments (jobs)
+        for job in sys.argv[1:]:
+            # check job is valid i.e. in jobs dictionary
+            if not job in jobs:
+                errorHandler(f"ERROR: job {job} does not exist", dateTimeStamp)
         else:
+            # check job refers to a vaild file or directory
             source =jobs[job]
             if not os.path.exists(source):
-                print(f"ERROR: source {source} does not exist")
+                errorHandler(f"ERROR: source {source} does not exist", dateTimeStamp)
             else:
+                # check backup location is a valid directory
                 destination = backupDir
                 if not os.path.exists(destination):
                     print(f"ERROR: destination {destination} does not exist")
                 else:
-                    dateTimeStamp = datetime.now().strftime("%Y%m%d-%H%M%S")
                     srcPath = pathlib.PurePath(source)
                     dstLoc = destination + "/" + srcPath.name + "-" + dateTimeStamp
+                    # backup directory or file as required
                     if pathlib.Path(source).is_dir():
                          shutil.copytree(source, dstLoc)
                     else:
                         shutil.copy2(source, dstLoc)
-                    pass
+                    
+                    
+                    #write success to log file
+                    writeLogMessage(f"Backed up {source} to {dstLoc}", dateTimeStamp, True)
 
 if __name__ == "__main__":
     main()
--- backupcfg.py
+++ backupcfg.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
-jobs = {"job700" : "/home/ec2-user/environment/ictpng302-vedtss-Luka-Baudewyns/test/file1",
-        "job1" : "/home/ec2-user/environment/ictpng302-vedtss-Luka-Baudewyns/test/dir1"}
+jobs = {"job1" : "/home/ec2-user/environment/ictpng302-vedtss-Luka-Baudewyns/test/file1",
+        "job2" : "/home/ec2-user/environment/ictpng302-vedtss-Luka-Baudewyns/test/dir1"}
         
 backupDir = "/home/ec2-user/environment/ictpng302-vedtss-Luka-Baudewyns/backups"
 
 backupLog = "/home/ec2-user/environment/ictpng302-vedtss-Luka-Baudewyns/backup.log"
 
-email = {}
+smtp = {"sender": "baudewynsluka@gmail.com",
+        "recipient": "baudewynsluka@gmail.com",
+        "server": "smtp.gmail.com",
+        "port": 587,
+        "user": "baudewynsluka@gmail.com", # need to specify a gmail email address with an app password
+        "password": ""} # need a gmail app password
